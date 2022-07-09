7a5928f0dd30f6e8a1e9a470bd4b470597811858
jj-2206 <jinju.woo2022@gmail.com>
2022-07-09 16:51:07 +0900

    fix: Add js libraries lodash/ gsap

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      // 배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
+// lodash 라이브러리의 throttle 기능, 300 = 0.3초
+// _.throttle(함수, 시간)
+// gsap 라이브러리 animation
+// gsap.to(요소, 지속시간, 옵션{});
